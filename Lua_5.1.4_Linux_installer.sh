a1e6905e07cb26c521f5de67cf639dc5a8ac531a
Guilherme Aquino Lima Barroso <guiaqui23@gmail.com>
2022-02-20 16:09:13 -0300

    Add files via upload

---
+++ Lua_5.1.4_Linux_installer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Instalando Lua 5.1.4
+sudo apt install build-essential libreadline-dev unzip
+curl -R -O http://www.lua.org/ftp/lua-5.1.4.tar.gz
+tar -zxf lua-5.1.4.tar.gz
+cd lua-5.1.4
+make linux test
+sudo make install
+#Instalando LuaRocks 3.8.0
+wget https://luarocks.org/releases/luarocks-3.8.0.tar.gz
+tar zxpf luarocks-3.8.0.tar.gz
+cd luarocks-3.8.0
+./configure --with-lua-include=/usr/local/include
+make -j$(nproc)
+sudo make install
+#Instalando alguns modulos básicos
+sudo luarocks install luacheck	#Lua linter
+sudo luarocks install busted		#Unit testing framework
+sudo luarocks install ldoc		#Gerador de documentação
